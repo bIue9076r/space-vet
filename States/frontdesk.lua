f55434d23fb3c0e6cc9ecbd4e9a9d99cf8224483
SHRIM <hypno.butter@gmail.com>
2026-07-14 11:36:59 -0500

    checkup and front desk

--- States/frontdesk.lua
+++ States/frontdesk.lua
@@ -5,7 +5,20 @@ G_STATE_FRONT_DESK_SUB = 1
 G_STATE_FRONT_DESK_SUBSTATES = {
 	[1] = {
 		Draw = function(self)
-			
+			love.graphics.setColor(229/255, 162/255, 85/255)
+			love.graphics.rectangle("fill",0,0,SCREEN_X,SCREEN_Y)
+            love.graphics.setColor(65/255, 38/255, 15/255)
+			love.graphics.rectangle("fill",0,SCREEN_Y - 100,SCREEN_X,100)
+
+			love.graphics.setColor(80/255,40/255,50/255)
+			-- computer desk
+			love.graphics.rectangle("fill",300,400,SCREEN_X - 600,100)
+
+			love.graphics.setColor(0,1,1)
+            -- computer
+            love.graphics.rectangle("fill",490,280,SCREEN_X - 800,120)
+
+			love.graphics.setColor(G_CLEAR)
 			Meta_Game.Draw()
 		end,
 
