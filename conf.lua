726c940934261a2c839adca18d69a773490609c7
bIue9076r <75851333+bIue9076r@users.noreply.github.com>
2026-07-22 23:44:30 -0500

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,6 +1,7 @@
 function love.conf(t)
 	t.version = "11.3"
-	t.console = true--false
+	t.console = false
+	-- t.console = true
 	t.window.width = 800
 	t.window.height = 600
 	t.window.minwidth = 800
@@ -23,7 +24,7 @@ function love.conf(t)
 	t.modules.mouse = true
 	t.modules.physics = false
 	t.modules.sound = true
-	t.modules.system = false
+	t.modules.system = true
 	t.modules.thread = false
 	t.modules.timer = true
 	t.modules.touch = false
